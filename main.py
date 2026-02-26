eeec5874176a8c5ebfc2cf1187e2783e792ed536
Alberto Sandoval <alberto.sandoval.950@my.csun.edu>
2026-02-25 20:01:45 -0800

    update

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ from shapely.geometry import box
 import rasterio
 from rasterio.features import rasterize
 from pygridmet.pygridmet import get_bygeom
+from rasterstats import zonal_stats
 
 
 #------------ DEFINING THE REGION --------------
-park = gpd.read_file("assests\\nps_boundary.shp")
-park = park.to_crs("EPSG:3310")
-geom = park.geometry.union_all()
+park = gpd.read_file("assests\\nps_boundary.shp").to_crs("EPSG:3310") # reading in boundry
+sequoia = park.loc[park['UNIT_CODE']=='SEQU'].copy() # extracts only sequoia
+geom = sequoia.geometry.union_all()
 
 #--------------- CREATING GRID -----------------
 # bounding box
-minx, miny, maxx, maxy = park.total_bounds
+minx, miny, maxx, maxy = sequoia.total_bounds
 
 cell_size = 500  # meters
 
@@ -23,10 +24,10 @@ for x in np.arange(minx, maxx, cell_size):
     for y in np.arange(miny, maxy, cell_size):
         cells.append(box(x, y, x + cell_size, y + cell_size))
 
-grid = gpd.GeoDataFrame(geometry=cells, crs=park.crs)
+grid = gpd.GeoDataFrame(geometry=cells, crs=sequoia.crs)
 
-# clip to park
-grid = grid[grid.intersects(park.union_all())].copy()
+# clip to sequoia
+grid = grid[grid.intersects(sequoia.union_all())].copy()
 
 # assign ids + centroids
 grid["cell_id"] = range(len(grid))
@@ -39,8 +40,7 @@ help(get_bygeom)
 ds = get_bygeom(
     geometry=geom,
     dates=("2020-07-01", "2020-07-10"),
-    crs=park.crs,
-    variables=["tmmx"],
-    conn_timeout=3000,          # seconds
-    validate_filesize=False     # reduces retries if server returns partial
+    crs=sequoia.crs,
+    variables=["tmmx"]
 )
+
